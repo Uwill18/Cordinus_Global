076e2fea8260a3e66150a345de90b4c72d8d4b9b
Hoshi Holiday <hoshiholiday@gmail.com>
2023-02-28 14:04:25 -0600

    Added two new views Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/cordinus/cordinus_global/controller/AddApptController.java
+++ src/main/java/cordinus/cordinus_global/controller/AddApptController.java
@@ -1,6 +1,62 @@
 package cordinus.cordinus_global.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 //https://www.youtube.com/watch?v=at4xyBOdgME
 //https://www.youtube.com/watch?v=i0j2AmsJQz0https://www.youtube.com/watch?v=i0j2AmsJQz0
+//https://www.youtube.com/watch?v=3Ht-JMQh2JI
 public class AddApptController {
+
+
+    @FXML
+    private DatePicker ApptEndPicker;
+
+    @FXML
+    private TextField ApptIDTxt;
+
+    @FXML
+    private DatePicker ApptStartPicker;
+
+    @FXML
+    private TextField ContactTxt;
+
+    @FXML
+    private TextField DescriptionTxt;
+
+    @FXML
+    private TextField LocationTxt;
+
+    @FXML
+    private TextField TitleTxt;
+
+    @FXML
+    private TextField TypeTxt;
+
+
+
+    @FXML
+    void InsertAppt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ApptScreenReturn(ActionEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("/cordinus/cordinus_global/AppointmentScreen.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Appointments");
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/main/java/cordinus/cordinus_global/controller/ModifyApptController.java
+++ src/main/java/cordinus/cordinus_global/controller/ModifyApptController.java
@@ -1,4 +1,44 @@
 package cordinus.cordinus_global.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+////https://www.youtube.com/watch?v=3Ht-JMQh2JI
 public class ModifyApptController {
+
+    @FXML
+    private DatePicker ApptEndPicker;
+
+    @FXML
+    private TextField ApptIDTxt;
+
+    @FXML
+    private DatePicker ApptStartPicker;
+
+    @FXML
+    private TextField ContactTxt;
+
+    @FXML
+    private TextField DescriptionTxt;
+
+    @FXML
+    private TextField LocationTxt;
+
+    @FXML
+    private TextField TitleTxt;
+
+    @FXML
+    private TextField TypeTxt;
+
+    @FXML
+    void ApptScreenReturn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UpdateAppt(ActionEvent event) {
+
+    }
 }
