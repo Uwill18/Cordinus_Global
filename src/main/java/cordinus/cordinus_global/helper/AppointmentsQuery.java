297febea4625dc1f7d38c3892be21604e573a4d7
Hoshi Holiday <hoshiholiday@gmail.com>
2023-03-08 18:15:15 -0600

    setup update Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/cordinus/cordinus_global/helper/AppointmentsQuery.java
+++ src/main/java/cordinus/cordinus_global/helper/AppointmentsQuery.java
@@ -33,11 +33,26 @@ public abstract class AppointmentsQuery {
 
     }
 
-    public static int update(int fruitId,String fruitName) throws SQLException {
-        String sql ="UPDATE FRUITS SET Fruit_Name = ? WHERE Fruit_ID = ?";
+    public static int update(String title, String description, String location, String type, Timestamp startby, Timestamp endby, String CreatedBy, Timestamp LastUpdate,String LastUpdatedBy, Timestamp CreatedDate,int customerid, int userid, int contact) throws SQLException {
+        String sql ="UPDATE APPOINTMENTS SET Title = ?,Description = ?,Location= ?, Type = ?, Start = ?, End = ?,Create_Date = ?, Created_By = ?, Last_Update = ?, Last_Updated_By = ?,Create_Date = ?, User_ID = ?, Contact_ID,  WHERE Customer_ID = ?";
         PreparedStatement ps = JDBC.connection.prepareStatement(sql);
-        ps.setString(1,fruitName);
-        ps.setInt(2,fruitId);
+        ps.setString(1,title);
+        ps.setString(2,description);
+        ps.setString(3,location);
+        ps.setString(4,type);
+        ps.setTimestamp(5,startby);//setTimestamp, valueof ldt
+
+        ps.setTimestamp(6,endby);//setTimestamp
+        ps.setTimestamp(7,CreatedDate);
+        ps.setString(8,CreatedBy);
+        ps.setTimestamp(9,LastUpdate);
+        ps.setString(10,LastUpdatedBy);
+        ps.setInt(11,customerid);
+        ps.setInt(12,userid);
+        ps.setInt(13,contact);
+
+        //System.out.println(ps);
+
         int rowsAffected = ps.executeUpdate();
         return rowsAffected;
 
