ef1b941d6b43e95a1742eadb5aeef6ea099ea993
Hoshi Holiday <hoshiholiday@gmail.com>
2023-03-11 21:02:12 -0600

    Login logic reviewed
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/cordinus/cordinus_global/controller/MainController.java
+++ src/main/java/cordinus/cordinus_global/controller/MainController.java
@@ -1,15 +1,25 @@
 package cordinus.cordinus_global.controller;
 
+import cordinus.cordinus_global.helper.JDBC;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 //import javafx.scene.control.Label;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.SimpleTimeZone;
 
 
 //This file is responsible for loading data applicable to Main
@@ -24,16 +34,20 @@ public class MainController {
     @FXML
     private TextField UsernameTxt;
 
+    @FXML
+    private Label CurrentTimeLbl;
+
 
 //Updated Main Menu Button Navigation
 
 
 //Password Validation//
-    //https://www.youtube.com/watch?v=1jiuM-gNyBc
+    //https://www.youtube.com/watch?v=1jiuM-gNyBc  > used
     //https://www.youtube.com/watch?v=Y8F-k925O-w  good
     //https://www.youtube.com/watch?v=wII6ufsn82c
 
     //up results being correct or incorrect use file i/o options to print them to a file
+    //in filewriter file, where i <  attempt sum (incorrect attempts, and correct attempts should sum less than 3)
 
 
 
@@ -49,31 +63,45 @@ public class MainController {
 
 //Main Menu Navigation//
     //ToLoginButton
+
+
+
+
 @FXML
 void LoginScreenButton(ActionEvent event) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("/cordinus/cordinus_global/LoginForm.fxml"));
     Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
     Scene scene = new Scene(fxmlLoader.load());
     stage.setTitle("Login Form");
+
     stage.setScene(scene);
+
     stage.show();
-
-
-
-
-
 
 
 }
     //MainMenuScreenButton
 
+    public void TimeLoad(){
+        LocalTime logintime = LocalTime.now();
+
+        System.out.println(logintime);
+        CurrentTimeLbl.setText(logintime.toString());
+    }
+
 
 
     @FXML
-    void MainMenuScreenButton(ActionEvent event) throws IOException {
+    void MainMenuScreenButton(ActionEvent event) throws IOException, SQLException {
 
         String username = UsernameTxt.getText();
         String password = PasswordTxt.getText();
+
+
+
+
+
+
 
         if((username.equals("test") && password.equals("test"))||(username.equals("admin") && password.equals("admin"))){
             FXMLLoader fxmlLoader = new FXMLLoader(MainController.class.getResource("/cordinus/cordinus_global/MainMenu.fxml"));
